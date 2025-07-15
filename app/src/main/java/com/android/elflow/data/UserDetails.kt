91c19f4040b989908207d86295feb4b5f731050a
Y Karthik reddy <karthik.reddy-yammanur>
2025-07-15 09:13:28 +0530

    Add shared preferences
    Added Login screen
    Added shared preferences to save user credentials.

---
+++ app/src/main/java/com/android/elflow/data/UserDetails.kt
@@ -0,0 +1,6 @@
+package com.android.elflow.data
+
+data class UserDetails(
+    val email: String,
+    val password: String
+)
---
+++ app/src/main/java/com/android/elflow/repository/PreferencesManager.kt
@@ -0,0 +1,27 @@
+package com.android.elflow.repository
+
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+
+class PreferencesManager(context: Context) {
+
+    companion object {
+        const val KEY_USER_EMAIL = "email"
+        const val KEY_PREFERENCES = "prefs"
+    }
+
+
+    private val sharedPreferences: SharedPreferences =
+        context.getSharedPreferences(KEY_PREFERENCES, Context.MODE_PRIVATE)
+
+    fun saveEmail(email:String){
+        sharedPreferences.edit { putString(KEY_USER_EMAIL, email) }
+    }
+
+    fun getEmail(): String{
+        return sharedPreferences.getString(KEY_USER_EMAIL, "") ?: ""
+    }
+
+
+}
